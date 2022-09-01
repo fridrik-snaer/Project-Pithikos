01aa12feecad9926cd69b0bf8a4d41e101c0e6bd
fridrik-snaer <fridriksnaer01@gmail.com>
2022-09-01 11:45:56 +0000

    initial commit

---
+++ src/main/java/is/hi/hbv501g/hbv1/Hbv1Application.java
@@ -0,0 +1,14 @@
+package is.hi.hbv501g.hbv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class Hbv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hbv1Application.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/hbv1/Hbv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.hbv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hbv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
